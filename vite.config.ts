bfa9e2b8dae91ed8291b1171c0bbf078a44fa528
JPX-0 <juliopariona2018@gmail.com>
2026-05-31 22:10:48 -0500

    v1.1.0

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
   plugins: [
+    netlify(),
     tsConfigPaths(),
     tailwindcss(),
     tanstackStart(),
